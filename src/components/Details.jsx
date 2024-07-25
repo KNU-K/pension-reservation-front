574b59cb6171c66b4e01cea0b58cacb6bcc06acd
KNU-K <knukang334@gmail.com>
2024-07-25 23:17:26 +0900

    fix : navbar 수정, main 도 반응형으로

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,151 +1,44 @@
+import React from "react";
+
 const Details = () => {
     return (
-        <div style={{ width: "600px" }} className="inset-0 bg-white p-6 overflow-auto">
-            <div doz_type="inside" className="inside">
-                <div doz_type="row" doz_grid="12" className="doz_row">
-                    <div doz_type="grid" doz_grid="12" className="col-dz col-dz-12">
-                        <div doz_type="widget" id="w20221004bd55ee8ba59e3">
-                            <div className="_widget_data _ds_animated_except" data-widget-name="여백" data-widget-type="padding" data-widget-parent-is-mobile="N">
-                                <div className="widget padding" data-height="30" style={{ marginTop: "px", marginBottom: "px" }}>
-                                    <div id="padding_w20221004bd55ee8ba59e3" style={{ width: "100%", minHeight: "1px", height: "30px" }}></div>
-                                </div>
+        <div className="w-full max-w-screen-lg mx-auto bg-white p-6 overflow-auto">
+            <div className="space-y-6">
+                {/* Spacer */}
+                <div className="h-8"></div>
+
+                {/* Text Section */}
+                <div className="text-center">
+                    <p className="text-2xl font-bold leading-relaxed">
+                        <span className="text-xl font-semibold">취호가 趣虎家</span>
+                    </p>
+                    <p className="text-lg italic leading-relaxed">"뜻을 취하는 호랑이 사원"</p>
+                    <p className="mt-4 text-base leading-relaxed">취 (趣)는 "마음이 쏠리는 방향"을 의미하며, 불가에서는 "지난 날에 지내온 업으로 인해 현재에 다다른다"는 의미가 있다.</p>
+                    <p className="mt-4 text-base leading-relaxed">
+                        호 (虎)는 "범"을 의미하며, 호랑이는 예로부터 산군이라 불리며 우리 민족의 신비한 힘이 있는 길상적인 의미를 갖는다. 또한 "옛날 마을에 큰 바위가 있었는데 호랑이가 자주 올라
+                        울었다" 하는 호명리와 일맥상통한다.
+                    </p>
+                    <p className="mt-4 text-base leading-relaxed">가 (家)는 "우리네들이 마음 놓고 편히 쉴 수 있는 공간"을 의미한다.</p>
+                    <p className="mt-4 text-base leading-relaxed">
+                        지난 날 지내온 삶의 방향이 흔들리는 (뜻을 이루지 못한)이들에게 온전한 쉼을 통해 스스로 뜻을 찾아갈 수 있는 "몸과 마음을 회복할 수 있는 호랑이 사원"을 만들고자 하였다.
+                    </p>
+                </div>
+
+                {/* Gallery Section */}
+                <div className="relative">
+                    <div className="overflow-hidden">
+                        <div className="relative h-80">
+                            <div className="absolute inset-0 bg-gray-200 flex items-center justify-center">
+                                <span className="text-gray-500">Gallery Placeholder</span>
                             </div>
+                            {/* Replace the placeholder with an actual image gallery */}
+                            <img src="https://cdn.imweb.me/thumbnail/20221007/88469c91a3862.jpeg" alt="Gallery" className="w-full h-full object-cover" />
                         </div>
                     </div>
                 </div>
-                <div doz_type="row" doz_grid="12" className="doz_row">
-                    <div doz_type="grid" doz_grid="12" className="col-dz col-dz-12">
-                        <div doz_type="widget" id="w20221001239fd81914d82">
-                            <div
-                                className="_widget_data"
-                                data-widget-name="텍스트"
-                                data-widget-type="text"
-                                data-widget-anim="none"
-                                data-widget-anim-duration="0.7"
-                                data-widget-anim-delay="0"
-                                data-widget-parent-is-mobile="N"
-                            >
-                                <div doz_type="text" className="widget _text_wrap widget_text_wrap fr-view default_padding" id="text_w20221001239fd81914d82">
-                                    <div className="text-table">
-                                        <div>
-                                            <p style={{ lineHeight: 2 }}>
-                                                <span style={{ fontSize: "20px" }}>
-                                                    <strong>취호가 趣虎家</strong>
-                                                </span>
-                                            </p>
-                                            <p style={{ lineHeight: 2 }}>
-                                                <span style={{ fontSize: "14px" }}>
-                                                    <em>"뜻을 취하는 호랑이 사원"</em>
-                                                </span>
-                                            </p>
-                                            <p>
-                                                <br />
-                                            </p>
-                                            <p>취 (趣)는 "마음이 쏠리는 방향"을 의미하며, 불가에서는 "지난 날에 지내온 업으로 인해 현재에 다다른다"는 의미가 있다.</p>
-                                            <p>
-                                                <br />
-                                            </p>
-                                            <p>호 (虎)는 "범"을 의미하며, 호랑이는 예로부터 산군이라 불리며 우리 민족의 신비한 힘이 있는 길상적인 의미를 갖는다.&nbsp;</p>
-                                            <p>
-                                                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;또한 "옛날 마을에 큰 바위가 있었는데 호랑이가 자주 올라 울었다" 하는 호명리와 일맥상통한다.
-                                                <br />
-                                                <br />
-                                            </p>
-                                            <p>
-                                                <br />
-                                            </p>
-                                            <p>가 (家)는 "우리네들이 마음 놓고 편히 쉴 수 있는 공간"을 의미한다.</p>
-                                            <p>
-                                                <br />
-                                            </p>
-                                            <p>지난 날 지내온 삶의 방향이 흔들리는 (뜻을 이루지 못한)이들에게 온전한 쉼을 통해 스스로 뜻을 찾아갈 수 있는&nbsp;</p>
-                                            <p>"몸과 마음을 회복할 수 있는 호랑이 사원"을 만들고자 하였다.</p>
-                                            <p>
-                                                <br />
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div doz_type="row" doz_grid="12" className="doz_row">
-                    <div doz_type="grid" doz_grid="12" className="col-dz col-dz-12">
-                        <div doz_type="widget" id="w202210079b1831e3037b0">
-                            <div
-                                className="_widget_data"
-                                data-widget-name="갤러리"
-                                data-widget-type="gallery2"
-                                data-widget-anim="none"
-                                data-widget-anim-duration="0.7"
-                                data-widget-anim-delay="0"
-                                data-widget-parent-is-mobile="N"
-                            >
-                                <div className="widget _gallery_wrap" style={{ height: "510px" }}>
-                                    <div
-                                        id="container_w202210079b1831e3037b0"
-                                        className="img_rendering slide_01 type_slide overlay_text container_border owl-carousel owl-theme owl-loaded gallery2 paging_type_dot paging_type_dot01 custom_nav nav_type_st00 nav_round"
-                                        style={{ paddingBottom: 0 }}
-                                    >
-                                        <div className="_item item_gallary" style={{ display: "", position: "relative" }} data-org="S202209305b814a94473b9/ee0942519e39c.jpeg">
-                                            <div id="caption_11487261" style={{ display: "none" }}>
-                                                <h4></h4>
-                                                <p></p>
-                                            </div>
-                                            <div className="item_container _item_container">
-                                                <div
-                                                    className="img_wrap _img_wrap no_content"
-                                                    id="gal_item_"
-                                                    style={{ backgroundImage: "url(https://cdn.imweb.me/thumbnail/20221007/88469c91a3862.jpeg)" }}
-                                                    data-bg="url(https://cdn.imweb.me/thumbnail/20221007/88469c91a3862.jpeg)"
-                                                    data-src="https://cdn.imweb.me/thumbnail/20221007/88469c91a3862.jpeg"
-                                                    data-sub-html="#caption_11487261"
-                                                    data-no="0"
-                                                ></div>
-                                                <div
-                                                    className="text_wrap _text_wrap _lightbox_item cursor_pointer"
-                                                    id="gal_item_11487261"
-                                                    data-src="https://cdn.imweb.me/thumbnail/20221007/88469c91a3862.jpeg"
-                                                    data-sub-html="#caption_11487261"
-                                                    style={{}}
-                                                >
-                                                    <p className="title"></p>
-                                                </div>
-                                                <div className="slide_overlay"></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div doz_type="row" doz_grid="12" className="doz_row">
-                    <div doz_type="grid" doz_grid="12" className="col-dz col-dz-12">
-                        <div doz_type="widget" id="w20221007243620f34094c">
-                            <div
-                                className="_widget_data"
-                                data-widget-name="텍스트"
-                                data-widget-type="text"
-                                data-widget-anim="none"
-                                data-widget-anim-duration="0.7"
-                                data-widget-anim-delay="0"
-                                data-widget-parent-is-mobile="N"
-                            >
-                                <div doz_type="text" className="widget _text_wrap widget_text_wrap fr-view default_padding" id="text_w20221007243620f34094c">
-                                    <div className="text-table">
-                                        <div>
-                                            <p style={{ textAlign: "right" }}>
-                                                <span style={{ color: "rgb(186, 183, 183)" }}>치유의 숲 평창 (김영일 作)</span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
+                {/* Text Section with attribution */}
+                <div className="text-right text-sm text-gray-500 mt-4">치유의 숲 평창 (김영일 作)</div>
             </div>
         </div>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,63 @@
 import React from "react";
+import { Link } from "react-router-dom"; // For routing
+import { Navbar, Nav, NavDropdown } from "react-bootstrap"; // Bootstrap components
 
-const Navbar = ({ toggleSidebar }) => {
+const NavBar = () => {
     return (
-        <nav className="bg-gray-500 p-4 fixed top-0 w-full z-10 flex items-center justify-between">
-            {/* 왼쪽 끝에 위치한 버튼 */}
-            <button className="text-white text-2xl lg:hidden" onClick={toggleSidebar}>
-                ☰
-            </button>
-
-            {/* 가운데에 위치한 제목 */}
-            <h1 className="text-white text-lg font-semibold">My Navbar</h1>
-
-            {/* 빈 공간을 만들어 제목과 버튼 사이에 여백을 추가 */}
-            <div className="flex-grow"></div>
-        </nav>
+        <Navbar className="bg-gray-100 py-2" expand="lg">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" className="border-none">
+                <span className="text-gray-700">☰</span>
+            </Navbar.Toggle>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto flex space-x-4">
+                    <Nav.Link as={Link} to="/" className="ml-4 text-gray-800 text-sm font-medium hover:text-gray-600" style={{ fontSize: "0.875rem", fontWeight: "500" }}>
+                        HOME
+                    </Nav.Link>
+                    <NavDropdown title="PROLOGUE" id="basic-nav-dropdown" className="text-sm">
+                        <NavDropdown.Item as={Link} to="/story" className="text-gray-800 hover:bg-gray-200">
+                            STORY
+                        </NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="/architecture" className="text-gray-800 hover:bg-gray-200">
+                            ARCHITECTURE
+                        </NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="/location" className="text-gray-800 hover:bg-gray-200">
+                            LOCATION
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link as={Link} to="#" className="text-gray-800 text-sm font-medium hover:text-gray-600" style={{ fontSize: "0.875rem", fontWeight: "500" }}>
+                        EXPERIENCE
+                    </Nav.Link>
+                    <NavDropdown title="ROOMS" id="basic-nav-dropdown-rooms" className="text-sm">
+                        <NavDropdown.Item as={Link} to="#" className="text-gray-800 hover:bg-gray-200">
+                            IN-HALE
+                        </NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="#" className="text-gray-800 hover:bg-gray-200">
+                            EX-HALE
+                        </NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="#" className="text-gray-800 hover:bg-gray-200">
+                            THE CALM
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="RESERVATIONS" id="basic-nav-dropdown-reservations" className="text-sm">
+                        <NavDropdown.Item as={Link} to="#" className="text-gray-800 hover:bg-gray-200">
+                            Reservation
+                        </NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="#" className="text-gray-800 hover:bg-gray-200">
+                            Confirm Reservation
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="INFORMATION" id="basic-nav-dropdown-information" className="text-sm">
+                        <NavDropdown.Item as={Link} to="#" className="text-gray-800 hover:bg-gray-200">
+                            마트
+                        </NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="#" className="text-gray-800 hover:bg-gray-200">
+                            음식점
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 };
 
-export default Navbar;
+export default NavBar;
